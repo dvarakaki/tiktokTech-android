63ed6f54129f03bef20b15c73fab35000f43ff20
dvarakaki <davi.arakaki@hotmail.com>
2026-09-23 10:17:31 -0300

    feat: Adicionar SplashActivity com logo de 2.5 segundos

--- app/src/main/java/com/aula/tiktoktech/MainActivity.java
+++ app/src/main/java/com/aula/tiktoktech/MainActivity.java
@@ -11,7 +11,6 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.splashscreen.SplashScreen;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -45,7 +44,6 @@ public class MainActivity extends AppCompatActivity implements PostsAdapter.Acoe
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
